4338c6c6cf34f98c684390e1917a3b01921bb9b0
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-12-03 15:38:20 -0200

    primeiro test

---
+++ src/test/java/br/com/caelum/agiletickets/FirstTest.java
@@ -0,0 +1,13 @@
+package br.com.caelum.agiletickets;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void hello() throws Exception {
+		assertTrue(true);
+	}
+}
