511044cc132edd66910ade2713878df6d88783c0
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-12-06 14:36:01 -0200

    Testes para a sessao

---
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -0,0 +1,23 @@
+package br.com.caelum.agiletickets.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SessaoTest {
+
+
+	@Test
+	public void deveVender1ingressoSeHa2vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(2);
+
+        Assert.assertTrue(sessao.podeReservar(1));
+	}
+	@Test
+	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+
+		Assert.assertFalse(sessao.podeReservar(3));
+	}
+}
