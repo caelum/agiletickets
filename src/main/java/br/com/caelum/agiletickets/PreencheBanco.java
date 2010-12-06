1b202cb414ee807df7d137650aa9d969ec77b5b9
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-12-06 18:52:24 -0200

    desinlineando PreencheBanco

--- src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
+++ src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
@@ -14,7 +14,11 @@ import br.com.caelum.vraptor.util.jpa.EntityManagerFactoryCreator;
 public class PreencheBanco {
 
 	public static void main(String[] args) {
-		EntityManager manager = createEntityManager();
+		EntityManagerFactoryCreator creator = new EntityManagerFactoryCreator();
+		creator.create();
+		EntityManagerCreator managerCreator = new EntityManagerCreator(creator.getInstance());
+		managerCreator.create();
+		EntityManager manager = managerCreator.getInstance();
 
 		manager.getTransaction().begin();
 		manager.createQuery("delete from Sessao").executeUpdate();
@@ -45,14 +49,5 @@ public class PreencheBanco {
 
 		manager.getTransaction().commit();
 		manager.close();
-	}
-
-	private static EntityManager createEntityManager() {
-		EntityManagerFactoryCreator creator = new EntityManagerFactoryCreator();
-		creator.create();
-		EntityManagerCreator managerCreator = new EntityManagerCreator(creator.getInstance());
-		managerCreator.create();
-		EntityManager manager = managerCreator.getInstance();
-		return manager;
 	}
 }
