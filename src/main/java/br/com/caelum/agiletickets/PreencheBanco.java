f2204191a2112d803336899680288abeaea1c19e
Mauricio Aniche <mauricioaniche@gmail.com>
2012-10-01 17:54:47 -0300

    comentariozinho

--- src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
+++ src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
@@ -13,6 +13,7 @@ import br.com.caelum.vraptor.util.jpa.EntityManagerFactoryCreator;
 
 public class PreencheBanco {
 
+	// ALUNO: Não apague essa classe
 	public static void main(String[] args) {
 		EntityManagerFactoryCreator creator = new EntityManagerFactoryCreator();
 		creator.create();
