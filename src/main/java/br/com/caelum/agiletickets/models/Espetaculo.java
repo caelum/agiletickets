0a8a1a74a3a8d8b461d696d813080f1dde14f920
¨Gabriel <gabrielrubens@gmail.com>
2013-10-26 16:54:40 -0200

    Adicionando logica para o caso de periodo semanal

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -2,7 +2,6 @@ package br.com.caelum.agiletickets.models;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -13,11 +12,13 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
 import br.com.caelum.agiletickets.exceptions.DataInicioSuperiorDataFimException;
+
 
 @Entity
 public class Espetaculo {
@@ -33,7 +34,7 @@ public class Espetaculo {
 	@Enumerated(EnumType.STRING)
 	private TipoDeEspetaculo tipo;
 
-	@OneToMany(mappedBy="espetaculo")
+	@OneToMany(mappedBy = "espetaculo")
 	private List<Sessao> sessoes = newArrayList();
 
 	@ManyToOne
@@ -84,58 +85,67 @@ public class Espetaculo {
 	}
 
 	/**
-      * Esse metodo eh responsavel por criar sessoes para
-      * o respectivo espetaculo, dado o intervalo de inicio e fim,
-      * mais a periodicidade.
-      * 
-      * O algoritmo funciona da seguinte forma:
-      * - Caso a data de inicio seja 01/01/2010, a data de fim seja 03/01/2010,
-      * e a periodicidade seja DIARIA, o algoritmo cria 3 sessoes, uma 
-      * para cada dia: 01/01, 02/01 e 03/01.
-      * 
-      * - Caso a data de inicio seja 01/01/2010, a data fim seja 31/01/2010,
-      * e a periodicidade seja SEMANAL, o algoritmo cria 5 sessoes, uma
-      * a cada 7 dias: 01/01, 08/01, 15/01, 22/01 e 29/01.
-      * 
-      * Repare que a data da primeira sessao é sempre a data inicial.
-      */
-	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
-		if (inicio.isAfter(fim)) {
-			throw new DataInicioSuperiorDataFimException();
-		}
+	 * Esse metodo eh responsavel por criar sessoes para o respectivo
+	 * espetaculo, dado o intervalo de inicio e fim, mais a periodicidade.
+	 * 
+	 * O algoritmo funciona da seguinte forma: - Caso a data de inicio seja
+	 * 01/01/2010, a data de fim seja 03/01/2010, e a periodicidade seja DIARIA,
+	 * o algoritmo cria 3 sessoes, uma para cada dia: 01/01, 02/01 e 03/01.
+	 * 
+	 * - Caso a data de inicio seja 01/01/2010, a data fim seja 31/01/2010, e a
+	 * periodicidade seja SEMANAL, o algoritmo cria 5 sessoes, uma a cada 7
+	 * dias: 01/01, 08/01, 15/01, 22/01 e 29/01.
+	 * 
+	 * Repare que a data da primeira sessao é sempre a data inicial.
+	 */
+	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim,
+			LocalTime horario, Periodicidade periodicidade) {
+		if (inicio.isAfter(fim)) throw new DataInicioSuperiorDataFimException();
+
 		int numeroDias = Days.daysBetween(inicio, fim).getDays();
-		List<Sessao> sessoes = new ArrayList<Sessao>();
-		for (int i = 0; i <= numeroDias; i++) {
-			Sessao sessao = new Sessao();
-			sessao.setInicio(inicio.toDateTime(horario).plusDays(i));
-			sessao.setEspetaculo(this);
-			sessoes.add(sessao);
+		
+		for (int i = 0; i <= numeroDias; i += periodicidade.getDias()) {
+			DateTime dia = inicio.toDateTime(horario).plusDays(i);
+			criaSessao(dia);
 		}
+		
 		return sessoes;
 	}
-	
+
+	private void criaSessao(DateTime dia) {
+		Sessao sessao = new Sessao();
+		sessao.setInicio(dia);
+		sessao.setEspetaculo(this);
+		sessoes.add(sessao);
+	}
+
 	public boolean vagas(int qtd, int min) {
-        // ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
-        int totDisp = 0;
+		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
+		int totDisp = 0;
 
-        for (Sessao s : sessoes) {
-            if (s.getIngressosDisponiveis() < min) return false;
-            totDisp += s.getIngressosDisponiveis();
-        }
+		for (Sessao s : sessoes) {
+			if (s.getIngressosDisponiveis() < min)
+				return false;
+			totDisp += s.getIngressosDisponiveis();
+		}
 
-        if (totDisp >= qtd) return true;
-        else return false;
-    }
+		if (totDisp >= qtd)
+			return true;
+		else
+			return false;
+	}
 
-    public boolean vagas(int qtd) {
-        // ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
-        int totDisp = 0;
+	public boolean vagas(int qtd) {
+		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
+		int totDisp = 0;
 
-        for (Sessao s : sessoes) {
-            totDisp += s.getIngressosDisponiveis();
-        }
+		for (Sessao s : sessoes) {
+			totDisp += s.getIngressosDisponiveis();
+		}
 
-        if (totDisp >= qtd) return true;
-        else return false;
-    }
+		if (totDisp >= qtd)
+			return true;
+		else
+			return false;
+	}
 }
--- src/main/java/br/com/caelum/agiletickets/models/Periodicidade.java
+++ src/main/java/br/com/caelum/agiletickets/models/Periodicidade.java
@@ -1,5 +1,14 @@
 package br.com.caelum.agiletickets.models;
 
 public enum Periodicidade {
-	DIARIA, SEMANAL
+	DIARIA(1), SEMANAL(7);
+	private int dias;
+	
+	Periodicidade(int dias){
+		this.dias = dias;
+	}
+	
+	public int getDias(){
+		return dias;
+	}
 }
