efb337c3d3c1d5863d8941c80df52e971e56542b
¨Gabriel <gabrielrubens@gmail.com>
2013-10-26 09:43:59 -0200

    refatorada a classe de teste

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,13 +1,19 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
-
+	private Sessao sessao;
+	
+	@Before
+	public void setUp(){
+		sessao = new Sessao();
+	}
+	
 	@Test
 	public void deveVenderTodosIngressos() throws Exception {
-		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
         Assert.assertTrue(sessao.podeReservar(2));
         Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
@@ -15,7 +21,6 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
@@ -23,7 +28,6 @@ public class SessaoTest {
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -31,7 +35,6 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
