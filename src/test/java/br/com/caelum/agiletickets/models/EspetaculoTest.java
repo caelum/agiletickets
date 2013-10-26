ce53b5757b9a60de86e9714ef4f1b838708ffb08
¨Gabriel <gabrielrubens@gmail.com>
2013-10-26 16:01:45 -0200

    Adicionando testes para criar 1 ou 2 sessoes

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -3,6 +3,11 @@ package br.com.caelum.agiletickets.models;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EspetaculoTest {
@@ -73,6 +78,41 @@ public class EspetaculoTest {
 		assertFalse(ivete.vagas(5, 3));
 	}
 
+	@Test
+	public void deveCriarUmaSessaoDiaria() {
+		Espetaculo espetaculo = new Espetaculo();
+		LocalDate inicio = new LocalDate(2013, 10, 26);
+		LocalDate fim = new LocalDate(2013, 10, 26);
+		LocalTime horario = new LocalTime(16, 10);
+		Periodicidade diaria = Periodicidade.DIARIA;
+		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, horario, diaria);
+		
+		Assert.assertEquals(1, sessoes.size());
+		Sessao sessao = sessoes.get(0);
+		Assert.assertEquals("26/10/13", sessao.getDia());
+		Assert.assertEquals("16:10", sessao.getHora());
+	}
+	
+	@Test
+	public void deveCriarDuasSessoesDiarias() {
+		Espetaculo espetaculo = new Espetaculo();
+		LocalDate inicio = new LocalDate(2013, 10, 26);
+		LocalDate fim = new LocalDate(2013, 10, 27);
+		LocalTime horario = new LocalTime(16, 10);
+		Periodicidade diaria = Periodicidade.DIARIA;
+		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, horario, diaria);
+		
+		Assert.assertEquals(2, sessoes.size());
+		
+		Sessao sessao = sessoes.get(0);
+		Assert.assertEquals("26/10/13", sessao.getDia());
+		Assert.assertEquals("16:10", sessao.getHora());
+		
+		sessao = sessoes.get(1);
+		Assert.assertEquals("27/10/13", sessao.getDia());
+		Assert.assertEquals("16:10", sessao.getHora());
+	}
+	
 	private Sessao sessaoComIngressosSobrando(int quantidade) {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(quantidade * 2);
