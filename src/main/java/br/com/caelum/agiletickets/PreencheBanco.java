7d37c303dd9d3d11b7b8a965d2a988d9ff831816
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2014-02-27 18:42:27 -0300

    Alterando dados das sessões.

--- src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
+++ src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
@@ -45,7 +45,7 @@ public class PreencheBanco {
 			sessao.setInicio(new DateTime().plusDays(7+i));
 			sessao.setDuracaoEmMinutos(60 * 3);
 			sessao.setTotalIngressos(100);
-			sessao.setIngressosReservados(i == 0 ? 100 : 10 - i);
+			sessao.setIngressosReservados(i < 5 ? 100 - i : 0);
 			sessao.setPreco(new BigDecimal("50"));
 			manager.persist(sessao);
 		}
